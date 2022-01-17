62e0692139eeb922c753bedc54567aa54eac23b2
ppaetz <ppaetz@stud.hs-bremen.de>
2022-01-17 11:44:40 +0000

    generating first carausius

---
+++ simulate.py
@@ -0,0 +1,20 @@
+import pybullet as p
+import pybullet_data
+import time
+
+physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0, 0, -9.8)
+planeId = p.loadURDF("plane.urdf")
+bodyId = p.loadURDF("urdf/Carausius_Morosus.urdf", 0, 0, 1)
+# p.loadSDF("world.sdf")
+
+for i in range(5000):    
+
+    i = 0
+    time.sleep(1/240)
+    
+    p.stepSimulation()
+    i += 1
+
+p.disconnect()
